f5309f288aa2d432185c3fa05b765bc33a0c908b
chtenb <ctenbrinke@gmail.com>
2022-12-29 01:31:42 +0100

    Partial day 14

--- src/Days/Day13.hs
+++ src/Days/Day13.hs
@@ -1,22 +1,10 @@
 module Days.Day13 (runDay) where
 
-{- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
-
-import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
 import Control.Applicative ((<|>))
-import Util.Util (DisplayString)
-{- ORMOLU_ENABLE -}
+import Data.Attoparsec.Text (Parser, decimal, sepBy, string)
+import Data.List (elemIndex, intercalate, sort)
+import Data.Maybe (fromJust)
+import Program.RunDay qualified as R (Day, runDay)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
@@ -50,7 +38,7 @@ instance Ord ListOrInt where
   compare (Int i) (Int j) = compare i j
   compare (Int i) (Lst l) = compare (Lst [Int i]) (Lst l)
   compare (Lst l) (Int i) = compare (Lst l) (Lst [Int i])
-  compare (Lst (x : xs)) (Lst (y : ys)) = if compare x y /= EQ then compare x y else compare (Lst xs) (Lst ys)
+  compare (Lst (x : xs)) (Lst (y : ys)) = if x /= y then compare x y else compare (Lst xs) (Lst ys)
   compare (Lst []) (Lst (y : ys)) = LT
   compare (Lst (x : xs)) (Lst []) = GT
   compare (Lst []) (Lst []) = EQ
@@ -60,7 +48,6 @@ instance Eq ListOrInt where
 
 type OutputA = Int
 
--- type OutputB = [(Int, ListOrInt)]
 type OutputB = Int
 
 ------------ PART A ------------
@@ -73,12 +60,10 @@ hasExpectedOrder (left, right) = left < right
 partB :: Input -> OutputB
 partB input =
   let inputPackets = uncurry (++) $ unzip input
+      divider1 = Lst [Lst [Int 2]]
+      divider2 = Lst [Lst [Int 6]]
       allPackets = divider1 : divider2 : inputPackets
       sortedPackets = sort allPackets
       index1 = fromJust $ elemIndex divider1 sortedPackets
       index2 = fromJust $ elemIndex divider2 sortedPackets
    in (index1 + 1) * (index2 + 1)
-
-divider1 = Lst [Lst [Int 2]]
-
-divider2 = Lst [Lst [Int 6]]
--- src/Days/Day14.hs
+++ src/Days/Day14.hs
@@ -1,3 +1,7 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use <$>" #-}
+{-# HLINT ignore "Avoid lambda" #-}
 module Days.Day14 (runDay) where
 
 {- ORMOLU_DISABLE -}
@@ -14,6 +18,12 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
+import Data.Array.ST
+import Control.Monad.ST
+import Data.Bifunctor (bimap)
+import Control.Monad (foldM, foldM_)
+import Util.Util
+import Data.Array
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,18 +31,89 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = parseRockPath `sepBy` "\n"
+  where
+    parseRockPath = parseCoord `sepBy` " -> "
+    parseCoord = do
+      x <- decimal
+      string ","
+      y <- decimal
+      return $ Coord x y
 
 ------------ TYPES ------------
-type Input = Void
+data Coord = Coord {x :: Int, y :: Int} deriving (Show, Eq)
 
-type OutputA = Void
+toTuple c = (c.x, c.y)
+
+fromTuple (x, y) = Coord x y
+
+instance Ord Coord where
+  compare a b = compare (toTuple a) (toTuple b)
+
+instance Ix Coord where
+  range coords = fromTuple <$> range (bimap toTuple toTuple coords)
+  inRange coords c = inRange (bimap toTuple toTuple coords) (toTuple c)
+  index coords c = index (bimap toTuple toTuple coords) (toTuple c)
+
+type RockPath = [Coord]
+
+type Input = [RockPath]
+
+type OutputA = DisplayString
 
 type OutputB = Void
 
+type Grid s = (STUArray s Coord Char)
+
+initialGrid :: forall s. Input -> ST s (Grid s)
+initialGrid input = do
+  let minX = (-1) + minimum (x <$> concat input)
+  let maxX = 1 + maximum (x <$> concat input)
+  let minY = 0 -- (-1) + minimum (y <$> concat input)
+  let maxY = 1 + maximum (y <$> concat input)
+  newListArray
+    (Coord minX minY, Coord maxX maxY)
+    (repeat '.')
+
+expandRockPath :: RockPath -> [Coord]
+expandRockPath [] = []
+expandRockPath [c] = [c]
+expandRockPath (from : to : cs) = expandRockLine ++ expandRockPath (to : cs)
+  where
+    range a b = [a, if a < b then a + 1 else a - 1 .. b]
+    expandRockLine
+      -- vertical
+      | from.x == to.x = (\y -> Coord from.x y) <$> range from.y to.y
+      -- horizontal
+      | from.y == to.y = (\x -> Coord x from.y) <$> range from.x to.x
+      | otherwise = error "not a straight line"
+
+drawRocks :: forall s. Grid s -> [Coord] -> ST s ()
+drawRocks grid coords = do
+  let writeCoord _ coord = writeArray grid coord '#'
+  foldM_ writeCoord () coords
+
+drawRockPath :: forall s. Grid s -> RockPath -> ST s ()
+drawRockPath grid path = drawRocks grid (expandRockPath path)
+
+drawRockPaths :: forall s. Grid s -> [RockPath] -> ST s ()
+drawRockPaths grid = foldM_ (const $ drawRockPath grid) ()
+
+drawGrid :: Array Coord Char -> [String]
+drawGrid grid =
+  let (minC, maxC) = bounds grid
+   in [[grid ! Coord x y | x <- [minC.x .. maxC.x]] | y <- [minC.y .. maxC.y]]
+
+displayGrid :: [String] -> DisplayString
+displayGrid lines = DisplayString $ intercalate "\n" lines
+
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input = runST $ do
+  grid <- initialGrid input
+  drawRockPaths grid input
+  result <- freeze grid
+  return $ displayGrid (drawGrid result)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
